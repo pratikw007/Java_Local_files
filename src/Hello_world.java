0ff62bf416aac6689e8c1fa6ba60c0a6d11c27e1
Pratik Waghmare <waghmarepratik287@gmail.com>
2022-08-20 16:14:30 +0530

    hello world

---
+++ src/Hello_world.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.Scanner;
+public class Hello_world {
+public static void main(String[] args){
+//    system.out.println("Hey How Are You?");
+    Scanner input = new Scanner(System.in);
+    System.out.println(input.nextInt());
+
+}
+}
+
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+package com.pratik;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+//        System.out.println("My Name Is pratik");
+//        Scanner input = new Scanner(System.in);
+//        System.out.println(input.nextInt());
+        int rollNo= 64; //4 bytes
+        char letter = 'p'; //  8 bytes
+        float marks = 94.43f; // 4 bytes
+        double largeDecimalNumber = 5582455.25658;// 8 bytes
+        long largeInteger = 484413648314L;  //8 bytes
+        boolean ckeck = false;
+    }
+}
+//56:33
